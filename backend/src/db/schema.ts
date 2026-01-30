198bfb91b16254e6b1d8d87a6dccd45f634f8fdf
b.chirani <chirani124@gmail.com>
2026-01-30 10:22:46 +0100

    Added DB and better auth

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,3 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import {user,session,verification,userRelations,account,accountRelations} from "../../auth-schema.js"
+export {user,session,verification,userRelations,account,accountRelations}
